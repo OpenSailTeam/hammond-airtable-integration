1f583a3f8bfc00f909b6b76f052007430aee24c1
OpenSailTeam <team@opensail.com>
2024-08-09 13:11:33 -0600

    update

--- services/webhookService.js
+++ services/webhookService.js
@@ -6,7 +6,7 @@ exports.createWebhook = async () => {
     const response = await axios.post(
       `https://api.airtable.com/v0/bases/${process.env.AIRTABLE_BASE_ID}/webhooks`,
       {
-        notificationUrl: "https://34.122.33.167:3000/airtable/webhook",
+        notificationUrl: "https://hammond.api.opensail.com:3000/airtable/webhook",
         specification: {
             options: {
               filters: {
