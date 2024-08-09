1c02b05ab0e10948cff9414be37393b9e663589f
OpenSailTeam <team@opensail.com>
2024-08-09 16:07:17 -0600

    error handle

--- services/airtableService.js
+++ services/airtableService.js
@@ -45,8 +45,8 @@ exports.syncQueueToExternalServices = async () => {
         } catch (error) {
             // Log the error
             // logger.log('error', `Error syncing record ${item.webhook.id}: ${error.message}`);
-            
-            throw new Error(`Failed to sync record ${item.webhook.id}: ${error.message}`);
+
+            throw new Error(`Failed to sync record`);
         }
     }
     // Clear the queue after processing
