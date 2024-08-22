634695b1a71003b4ff4f98f7fb25c223737aca25
OpenSailTeam <team@opensail.com>
2024-08-22 13:35:22 -0600

    update queue

--- utils/queue.js
+++ utils/queue.js
@@ -1,11 +1,7 @@
 let queue = [];
 
 exports.push = (item) => {
-    const exists = queue.some(existingItem => existingItem.id === item.id);
-
-    if (!exists) {
         queue.push(item);
-    }
 };
 
 exports.getAll = () => {
