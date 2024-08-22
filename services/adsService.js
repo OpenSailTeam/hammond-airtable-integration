f17ec468feb428642a32d9545f11d33f0d6f269f
OpenSailTeam <team@opensail.com>
2024-08-22 17:18:51 -0600

    No commit message

--- services/adsService.js
+++ services/adsService.js
@@ -239,11 +239,16 @@ module.exports = {
           {
             asset_operation: {
               create: {
-                dynamic_real_estate_asset: fieldData
+                dynamic_real_estate_asset: {
+                  "listing_id": '12345',
+                  "listing_name": 'Test',
+                  "final_url": 'https://hammondrealty.ca/properties-new'
+                }
               }
             }
           }
-        ]
+        ],
+        partial_failure: false,
       });
 
       console.log('Update response:', response);
