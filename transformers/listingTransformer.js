476e1c5562633269eff3ef900c7ebf8d13222adc
OpenSailTeam <team@opensail.com>
2024-08-22 18:29:51 -0600

    name format

--- transformers/listingTransformer.js
+++ transformers/listingTransformer.js
@@ -5,7 +5,7 @@ module.exports = {
         final_urls: [slugToUrl(internalObject["fldkkNCldg9MufIKi"])],
         dynamic_real_estate_asset: {
             listing_id: internalObject["fldsSdgGBKmG3Stvi"],
-            listing_name: internalObject["fldw8YroAQYa4eja5"],
+            listing_name: trimString(internalObject["fldw8YroAQYa4eja5"], 23),
             city_name: internalObject["fldc7RlwdErEiIli1"]["valuesByLinkedRecordId"][internalObject["fldc7RlwdErEiIli1"]["linkedRecordIds"][0]][0],
             description: internalObject["fldWz4IkS9h1Nfdr4"],
             property_type: "farmland",
@@ -24,6 +24,10 @@ function slugToUrl(slug) {
 
   function formatPrice(number) {
     return number.toString() + " CAD";
+  }
+
+  function trimString(string, length) {
+    return string.substring(0, length);
   }
 
 
