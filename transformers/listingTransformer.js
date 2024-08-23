86bdb8a25b6422335d28d2431175002a9fa3fbeb
OpenSailTeam <team@opensail.com>
2024-08-22 18:50:24 -0600

    ok

--- transformers/listingTransformer.js
+++ transformers/listingTransformer.js
@@ -1,10 +1,21 @@
 module.exports = {
 
-    transformToAdsFormat: (internalObject) => {
-        return {
-            nothing: "nothing"};
-    }
-    
+transformToAdsFormat: (internalObject) => {
+    return {
+        final_urls: [slugToUrl(internalObject["fldkkNCldg9MufIKi"])],
+        dynamic_real_estate_asset: {
+            listing_id: internalObject["fldsSdgGBKmG3Stvi"],
+            listing_name: trimString(internalObject["fldw8YroAQYa4eja5"], 23),
+            city_name: internalObject["fldc7RlwdErEiIli1"]["valuesByLinkedRecordId"][internalObject["fldc7RlwdErEiIli1"]["linkedRecordIds"][0]][0],
+            description: internalObject["fldWz4IkS9h1Nfdr4"],
+            property_type: "farmland",
+            listing_type: "sale",
+            address: internalObject["fldpjQcD4ABxDFmIZ"],
+            price: formatPrice(internalObject["fld3OAds8z2vDEnqz"])
+        }
+    };
+}
+
   
 }
 
@@ -19,48 +30,3 @@ function slugToUrl(slug) {
   function trimString(string, length) {
     return string.substring(0, length);
   }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  module.exports = {
-
-    transformToAdsFormat: (internalObject) => {
-      return {
-          "listing_id": internalObject["fldsSdgGBKmG3Stvi"],
-          "listing_name": internalObject["fldw8YroAQYa4eja5"],
-          "final_url": slugToUrl(internalObject["fldkkNCldg9MufIKi"]),
-          "city_name": internalObject["fldc7RlwdErEiIli1"]["valuesByLinkedRecordId"][internalObject["fldc7RlwdErEiIli1"]["linkedRecordIds"][0]][0],
-          "description": internalObject["fldWz4IkS9h1Nfdr4"],
-          "property_type": "farmland",
-          "listing_type": "sale",
-          //"contextual_keywords": [],
-          "address": internalObject["fldpjQcD4ABxDFmIZ"],
-          "price": internalObject["fld3OAds8z2vDEnqz"].toString(),
-          //"formatted_price": internalObject,
-          //"tracking_template": internalObject,
-          //"final_mobile_URL": internalObject,
-          "image_URL": internalObject["fldsaCnr0pnZMh2k7"][0]["url"],
-          //"android_app_link": internalObject,
-          //"ios_app_link": internalObject,
-          //"ios_app_store_id": internalObject,
-          //"similar_listing_ids": internalObject
-      };
-    }
-    
-  }
-  
-  function slugToUrl(slug) {
-      return "https://hammondrealty.ca/listings/" + slug;
-    }
