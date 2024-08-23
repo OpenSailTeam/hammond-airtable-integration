e79d23a8384b3671c21f3e1c18f8fd1e34f28bb7
OpenSailTeam <team@opensail.com>
2024-08-22 18:46:33 -0600

    huh

--- transformers/listingTransformer.js
+++ transformers/listingTransformer.js
@@ -1,20 +1,21 @@
 module.exports = {
 
-  transformToAdsFormat: (internalObject) => {
-    return {
-        final_urls: [slugToUrl(internalObject["fldkkNCldg9MufIKi"])],
-        dynamic_real_estate_asset: {
-            listing_id: internalObject["fldsSdgGBKmG3Stvi"],
-            listing_name: trimString(internalObject["fldw8YroAQYa4eja5"], 23),
-            city_name: internalObject["fldc7RlwdErEiIli1"]["valuesByLinkedRecordId"][internalObject["fldc7RlwdErEiIli1"]["linkedRecordIds"][0]][0],
-            description: internalObject["fldWz4IkS9h1Nfdr4"],
-            property_type: "farmland",
-            listing_type: "sale",
-            address: internalObject["fldpjQcD4ABxDFmIZ"],
-            price: formatPrice(internalObject["fld3OAds8z2vDEnqz"])
-        }
-    };
-  }
+    transformToAdsFormat: (internalObject) => {
+        return {
+            final_urls: [slugToUrl(internalObject["fldkkNCldg9MufIKi"])],
+            dynamic_real_estate_asset: {
+                listing_id: internalObject["fldsSdgGBKmG3Stvi"],
+                listing_name: trimString(internalObject["fldw8YroAQYa4eja5"], 23),
+                city_name: internalObject["fldc7RlwdErEiIli1"]["valuesByLinkedRecordId"][internalObject["fldc7RlwdErEiIli1"]["linkedRecordIds"][0]][0],
+                description: internalObject["fldWz4IkS9h1Nfdr4"],
+                property_type: "farmland",
+                listing_type: "sale",
+                address: internalObject["fldpjQcD4ABxDFmIZ"],
+                price: formatPrice(internalObject["fld3OAds8z2vDEnqz"])
+            }
+        };
+    }
+    
   
 }
 
