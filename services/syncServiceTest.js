04dcb5ae4bd6644a0ce6437bfc26d18c5984d68c
OpenSailTeam <team@opensail.com>
2024-08-23 07:10:56 -0600

    No commit message

--- services/syncServiceTest.js
+++ services/syncServiceTest.js
@@ -15,36 +15,10 @@ module.exports = {
       try {
       console.log("Field data:", fieldData);
         await adsService.createListing(fieldData);
-        console.log(`Successfully created Google Ads asset and updated Airtable record ${recordId}`);
+        console.log(`Successfully created Google Ads asset and updated Airtable record ${record.id}`);
       } catch (error) {
-        console.error(`Error handling created record ${recordId}:`, error.response ? error.response.data : error.message);
+        console.error(`Error handling created record ${record.id}:`, error.response ? error.response.data : error.message);
       }
     }
-
-    // Handle Changed Records
-    if (payload.changedRecordsById) {
-        for (const [recordId, recordData] of Object.entries(payload.changedRecordsById)) {
-          const fieldData = listingTransformer.transformToAdsFormat(recordId, recordData.current.cellValuesByFieldId);
-  
-          try {
-            await adsService.updateListingDataById(recordId, fieldData);
-            console.log(`Successfully updated Google Ads asset ${recordId}`);
-          } catch (error) {
-            console.error(`Error updating record ${recordId}:`, error.response ? error.response.data : error.message);
-          }
-        }
-      }
-  
-      // Handle Destroyed Records
-      if (payload.destroyedRecordIds) {
-        for (const recordId of payload.destroyedRecordIds) {
-  
-          try {
-            await adsService.removeListingById(recordId);
-          } catch (error) {
-            console.error(`Error deleting record ${recordId}:`, error.response ? error.response.data : error.message);
-          }
-        }
-      }
   }
 };
